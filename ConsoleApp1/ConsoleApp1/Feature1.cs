96abb84539f70bc9f9bfbe767922278bbb661d3f
Ron Stubbington <rstubbington@nexgenrx.com>
2018-03-28 21:53:18 -0400

    Add code to add.

---
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Feature1
+    {
+        public int add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+            
+        }
+    }
+}
